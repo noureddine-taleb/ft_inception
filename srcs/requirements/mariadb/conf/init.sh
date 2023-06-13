a401f473ee0acecce8bf72ef221dfbf14d707660
ntaleb <ntaleb@1337.ma>
2023-06-13 12:56:46 -0400

    mariadb is working

---
+++ srcs/requirements/mariadb/conf/init.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+DATADIR=/var/lib/mysql
+USER=mysql
+
+DB=wordpress
+DBUSER=wpman
+DBUSERPASSWORD='P@ssw0rd'
+
+# create data dir if it doesn't exists this happens when we run for the first time
+if [ ! -d $DATADIR/mysql ]
+then
+	echo ----- init db dir
+	mariadb-install-db --datadir=$DATADIR --rpm --auth-root-authentication-method=normal \
+		--skip-test-db \
+		--old-mode='UTF8_IS_UTF8MB3' \
+		--default-time-zone=SYSTEM --enforce-storage-engine= \
+		--skip-log-bin \
+		--expire-logs-days=0 \
+		--loose-innodb_buffer_pool_load_at_startup=0 \
+		--loose-innodb_buffer_pool_dump_at_shutdown=0
+	chown -R $USER $DATADIR;
+
+	echo ------- starting mariadb server
+	mariadbd --user $USER &
+
+	MARIADBPID=$!
+
+	echo --------- wait for mariadb server to start
+	sleep 10
+
+	echo --------- create $DB database
+	echo "create database $DB;" | mariadb -u root 
+
+	echo --------- create $DBUSER user
+	echo "grant all privileges on $DB.* to $DBUSER@'%' identified by '$DBUSERPASSWORD';" | mariadb -u root 
+
+	echo ------- foreground mariadb
+	wait $MARIADBPID
+else
+	chown -R $USER $DATADIR;
+	mariadbd --user $USER
+fi
